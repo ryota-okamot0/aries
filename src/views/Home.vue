dc534ecb3b510173925eec25056debff36b983ed
ryota.okamoto <r.okamoto@ridecore.co.jp>
2023-02-28 23:33:14 +0900

    add page link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <!-- template -->
 <template>
   <div>
-    <h1>ホーム</h1>
+    <p>ホーム画面</p>
+    <p><router-link to="sign-in">サインイン</router-link></p>
+    <p><button @click="calendar">カレンダー</button></p>
+    <p><button @click="profile">プロフィール</button></p>
+    <p><button @click="share">共有</button></p>
   </div>
 </template>
 
@@ -10,5 +14,33 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class HomeComponent extends Vue {}
+export default class HomeComponent extends Vue {
+  private calendar() {
+    this.$router.push(
+      "calendar/month",
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => {},
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => {}
+    );
+  }
+  private profile() {
+    this.$router.push(
+      "profile",
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => {},
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => {}
+    );
+  }
+  private share() {
+    this.$router.push(
+      "share",
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => {},
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => {}
+    );
+  }
+}
 </script>
